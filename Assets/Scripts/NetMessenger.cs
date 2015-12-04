f989db8e7bb1bb30a3438c7400e1e225048293b8
Sharat Bhat <sharat@firehosegames.com>
2015-12-04 16:18:55 -0500

    Updated socket setup with program treated as the server

--- Assets/Scripts/NetMessenger.cs
+++ Assets/Scripts/NetMessenger.cs
@@ -11,46 +11,101 @@ public class NetMessenger : MonoBehaviour
     public NetMQContext ctx;
     public NetMQ.Sockets.ResponseSocket server = null;
     public NetMQ.Sockets.RequestSocket client = null;
+
+    private NetMQMessage lastMessage = new NetMQMessage();
+    private NetMQMessage lastMessageSent = new NetMQMessage();
+#endregion
+
+#region Const message values
+    // To Send From Server
+    const string MSG_S_ConfirmClientJoin = "CLIENT_INIT";
+    const string MSG_S_FrameData = "FRAME_UPDATE";
+
+    // To Receive From Client
+    const string MSG_R_ClientJoin = "CLIENT_JOIN";
+    const string MSG_R_FrameInput = "CLIENT_INPUT";
 #endregion
 
 #region Unity callbacks
     void Start()
     {
         ctx = NetMQContext.Create();
-        Connect();
+        CreateServer();
+#if UNITY_EDITOR
+        CreateTestClient();
         Test();
+#endif
     }
     
     void Update()
     {
+        if (server != null)
+        {
+            if (server.TryReceiveMultipartMessage(ref lastMessage))
+                HandleFrameMessage();
+        }
     }
 #endregion
 
-    private void Connect()
+    private void CreateServer()
     {
-        // TODO: set up connection properly
-#if UNITY_EDITOR
+        // TODO: Set up in a way that allows for multiple clients
+        // We'd need separate sockets for each client
         server = ctx.CreateResponseSocket();
         server.Bind("tcp://127.0.0.1:5556");
-#endif
-        client = ctx.CreateRequestSocket();
-        client.Connect("tcp://127.0.0.1:5556");
     }
 
+    private void CreateTestClient()
+    {
+        client = ctx.CreateRequestSocket();
+        client.Connect("tcp://127.0.0.1:5556");
+        client.SendFrame("Hello");
+    }
+
+#region Receive messages from the client
+    public void HandleFrameMessage()
+    {
+        string msgHeader = lastMessage.First.ToString();
+
+        switch(msgHeader.ToString())
+        {
+            case MSG_R_ClientJoin:
+                OnClientJoin(lastMessage);
+                break;
+            case MSG_R_FrameInput:
+                RecieveClientInput(lastMessage);
+                break;
+        }
+    }
+
+    public void RecieveClientInput(NetMQMessage msg)
+    {
+        // TODO: Ferry input to correct location
+    }
+
+    public void OnClientJoin(NetMQMessage msg)
+    {
+        lastMessageSent.Clear();
+        lastMessageSent.Append(MSG_S_ConfirmClientJoin);
+        client.SendMultipartMessage(lastMessageSent);
+    }
+#endregion
+    
     public void SendFrameUpdate(CameraStreamer.CaptureRequest streamCapture)
     {
-        NetMQMessage msg = new NetMQ.NetMQMessage();
+        lastMessageSent.Clear();
+        lastMessageSent.Append(MSG_S_FrameData);
         // TODO: Additional frame message description?
-
+        
         // Add in captured frames
         int numValues = Mathf.Min(streamCapture.shadersList.Count, streamCapture.retValue.Count);
         for(int i = 0; i < numValues; ++i)
-            msg.Append(streamCapture.retValue[i].pictureBuffer);
-
+            lastMessageSent.Append(streamCapture.retValue[i].pictureBuffer);
+        
         // TODO: Insert other wanted data
-
+        
         // Send out the real message
-        client.SendMultipartMessage(msg);
+        client.SendMultipartMessage(lastMessageSent);
     }
 
     private void Test()
