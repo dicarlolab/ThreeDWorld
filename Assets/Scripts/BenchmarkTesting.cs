6f4b102cce383eea6da9d3c9ea0ece191fa22c90
jazzguitarist <richard@theoates.com>
2016-01-19 18:23:12 -0500

    Adding Benchmark Test Updates
    Main Scene currently dropping lots of couches, uncheck BenchmarkTest
    object’s script “BenchmarkTesting” to stop test.

--- Assets/Scripts/BenchmarkTesting.cs
+++ Assets/Scripts/BenchmarkTesting.cs
@@ -17,8 +17,9 @@ public class BenchmarkTesting : MonoBehaviour {
 	private int iterationCount = 0;
 	// list with domain covering number of couches, range covering fps
 	//private float[,] framerates;
-	// initial coords of sofa
-	private Vector3 initalSofaPosition;
+	// initial coords, and rotation of sofa
+	private Vector3[] initialSofaPosition = new Vector3[5];
+	private Quaternion[] initialSofaRotation = new Quaternion[5];
 	// height of sofa
 	private float sofaHeight;
 	// time it takes for current iteration to complete
@@ -36,6 +37,10 @@ public class BenchmarkTesting : MonoBehaviour {
 		sofaScaledSeparation = 1.5f;
 		sofaBlue = GameObject.Find("sofa_blue");
 		sofaHeight = 4.0f;
+		for (int i = 0; i < 5; i++){
+			initialSofaPosition[i] = sofaBlue.transform.GetChild (i).position;
+			initialSofaRotation[i] = sofaBlue.transform.GetChild (i).rotation;
+		}
 		fallDuration = calculateFallDuration();
 		iterationStartTime = 0;
 	}
@@ -50,20 +55,22 @@ public class BenchmarkTesting : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		// if 200% of the time passed for highest couch to reach the floor,
+		// if twice the time passed for highest couch to reach the floor,
 		// run new iteration with deltaSofas sofas than last iteration unless
 		// maxIterations
 		if (iterationCount < maxIterations && Time.time - iterationStartTime > fallDuration) {
 
 			iterationCount++;
 
-			print (Time.time - iterationStartTime);
+			// reset main sofa to initial state
+			ResetMainSofa();
+
+			// get rid of clone sofas
 			foreach (GameObject obj in GameObject.FindGameObjectsWithTag("sofa test")) {
 				Destroy (obj);
 			}
 
-			Instantiate (sofaBlue);
-
+			// create new sofa clones with tag "sofa test" indicating they are clones and place them some distance above each other
 			for (int i = 0; i < deltaSofas * iterationCount; i++) {
 				GameObject sofaBlueClone = Instantiate(sofaBlue);
 				sofaBlueClone.tag = "sofa test";
@@ -73,6 +80,21 @@ public class BenchmarkTesting : MonoBehaviour {
 			// set new fall duration to if sofa were 5*1.5 sofas higher than current iteration
 			fallDuration = calculateFallDuration();
 			iterationStartTime = Time.time;
+		}
+	}
+
+	// puts the sofa back to initial position and rotation with no velocities
+	void ResetMainSofa() {
+		for (int i = 0; i < 5; i++){
+			Rigidbody rb = sofaBlue.transform.GetChild(i).GetComponent<Rigidbody> ();
+			sofaBlue.transform.GetChild(i).position = initialSofaPosition[i];
+			sofaBlue.transform.GetChild(i).rotation = initialSofaRotation[i];
+			if (rb != null) {
+				rb.velocity = Vector3.zero;
+				rb.angularVelocity = Vector3.zero;
+			} else {
+				Debug.LogError("Main Sofa Rigidbody does not exist :(");
+			}
 		}
 	}
 }
