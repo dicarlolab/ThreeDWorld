3d06db05ae30843738e007cfa09f1a57432437ad
jazzguitarist <richard@theoates.com>
2016-08-11 10:09:37 -0400

    undid unintentional change

--- Assets/Scripts/ProceduralGeneration.cs
+++ Assets/Scripts/ProceduralGeneration.cs
@@ -609,9 +609,6 @@ public class ProceduralGeneration : MonoBehaviour
 			info = prefabList[_rand.Next(0, prefabList.Count)];
         }
 
-		if (info.fileName.Equals ("Prefabs/Props/Lights/lamp_18"))
-			Debug.Log("Investigate bug");
-
         // Find a spot to place this object
         int spawnX, spawnZ;
         float modScale;
