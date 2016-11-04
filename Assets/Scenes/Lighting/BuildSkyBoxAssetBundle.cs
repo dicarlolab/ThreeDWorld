61f9f75c4bde9dca20ff74580ca1201e185648ee
Damian Mrowca <damian@DN0a221825.SUNet>
2016-11-04 12:05:31 -0700

    Development for beautiful rendering

---
+++ Assets/Scenes/Lighting/BuildSkyBoxAssetBundle.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+
+public class CreateAssetBundles
+{
+    [MenuItem ("Assets/Build SkyBox Bundle")]
+    static void BuildAllAssetBundles ()
+    {
+        BuildPipeline.BuildAssetBundles ("Assets/Scenes/Lighting", BuildAssetBundleOptions.None, BuildTarget.StandaloneOSXUniversal);
+    }
+}
