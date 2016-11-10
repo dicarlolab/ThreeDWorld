ab810df58031c4ec6be65454301822e3d7d43141
zcx_win8 <nbfhzcx002@163.com>
2016-11-10 00:21:21 -0800

    cmdRun for generating information through cmdRun

---
+++ ServerTools/mongodb_test.py
@@ -0,0 +1,7 @@
+import pymongo
+conn = pymongo.MongoClient(port=22334)
+coll = conn['synthetic_generative']['3d_models']
+test_coll = coll.find({'type': 'shapenet', 'version': 2, 'has_texture':True})
+
+for i in range(10):
+    print(test_coll[i]['id'])
