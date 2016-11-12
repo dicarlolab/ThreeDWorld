eacd68c1a2748227039aaac3a137f1b7bc127af8
chengxuz@yaminlab1.stanford <chengxuz@stanford.edu>
2016-11-11 19:25:57 -0800

    Add actual info files in kanefsky

--- ServerTools/cmdRun_getInfo_batch.py
+++ ServerTools/cmdRun_getInfo_batch.py
@@ -23,12 +23,14 @@ if __name__ == "__main__":
     max_lengthn     = min(test_coll.count(), options.startn + options.lengthn)
 
     if options.windowsflag==0:
-        shapenet_prefix = '/mnt/data/threedworld_related/ShapeNetCore.v2'
-        project_path    = '/home/chengxuz/test_empty_all/test_empty_project_' + str(options.indexn) 
+        bundle_path     = '/home/chengxuz/test_empty_all/bundle_all'
+        txtout_path     = '/home/chengxuz/test_empty_all/text_all'
+        project_path    = '/home/chengxuz/test_empty_all/test_empty_project_' + str(options.indexn)
         original_proj   = '/home/chengxuz/test_empty_project '
         unity_path      = '/opt/Unity/Editor/Unity'
-        bundle_path     = '/home/chengxuz/test_empty_all/bundle_all/'
+        output_path     = 'Assets/PrefabDatabase/Info_for_' + str(options.startn) + '_' + str(options.lengthn) + '.txt'
     else:
+        # Not supported yet
         shapenet_prefix = 'C:/Users/threed/Documents/shapenet/ShapeNetCore.v2'
         project_path    = 'C:/Users/threed/ThreeDWorld_related/test_empty_project/test_empty_project_' + str(options.indexn) 
         original_proj   = 'C:/Users/threed/ThreeDWorld_related/test_empty_project/test_empty_project '
@@ -36,20 +38,19 @@ if __name__ == "__main__":
         bundle_path     = 'C:/Users/threed/ThreeDWorld_related/test_empty_project/all_assetbundles'
 
 
-    target_prefix   = project_path + '/Assets/Models/sel_objs/temp_for_cmdRun'
-    target_bund_pre = project_path + '/Assets/PrefabDatabase/AssetBundles/Separated/'
+    target_prefix   = project_path + '/Assets/PrefabDatabase/AssetBundles/bundle_part/'
 
     os.system('cp -r ' + original_proj + project_path)
 
     for index_tmp in range(options.startn, max_lengthn):
         shapenet_info_tmp = test_coll[index_tmp]
-        new_path    = os.path.join(shapenet_prefix, shapenet_info_tmp['shapenet_synset'][1:], shapenet_info_tmp['id'])
+        new_path    = os.path.join(bundle_path, shapenet_info_tmp['id'] + '.bundle')
         #print(new_path)
-        os.system('cp -r ' + new_path + ' ' + target_prefix)
+        os.system('cp ' + new_path + ' ' + target_prefix)
     
     #cmd_str         = unity_path + ' -batchmode -quit -projectPath ' + project_path + ' -executeMethod CreatePrefabCMD.CreatePrefabFromModel -nographics -logFile -outputDir Assets/Models/sel_objs/temp_for_cmdRun/'
-    cmd_str         = unity_path + ' -batchmode -quit -projectPath ' + project_path + ' -executeMethod CreatePrefabCMD.CreatePrefabFromModel -nographics -outputDir Assets/Models/sel_objs/temp_for_cmdRun/'
+    cmd_str         = unity_path + ' -batchmode -quit -projectPath ' + project_path + ' -executeMethod CreatePrefabCMD.LoadInfoFromBundle -nographics -inputDir Assets/PrefabDatabase/AssetBundles/bundle_part/ -outputFile ' + output_path
     print(cmd_str)
     os.system(cmd_str)
-    os.system('mv ' + target_bund_pre + '*.bundle ' + bundle_path)
+    os.system('mv ' + os.path.join(project_path , output_path) + ' ' + txtout_path)
     os.system('rm -r ' + project_path)
