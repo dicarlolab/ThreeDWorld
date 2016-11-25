db0049576da8e40cfe3a34cfc74b15b4eabbfc7e
Damian Mrowca <mrowca@gmx.de>
2016-11-24 21:43:21 -0800

    Material property extractor bug fix

--- Assets/Scripts/Editor/MaterialPropertyExtractor.cs
+++ Assets/Scripts/Editor/MaterialPropertyExtractor.cs
@@ -37,16 +37,30 @@ public class MaterialPropertyExtractor {
 			// Name of the imported material
 			string name = mat.name.Split(new string[] {"_MAT_"}, StringSplitOptions.None)[1];
 
+			// Albedo Color
+			float[] Color = new float[4];
+			Color[0] = mat.GetColor("_Color").r;
+			Color[1] = mat.GetColor("_Color").g;
+			Color[2] = mat.GetColor("_Color").b;
+			Color[3] = mat.GetColor("_Color").a;
+
 			// Specular Color
+			mat.shader = Shader.Find("Standard (Specular setup)");
 			float[] specColor = new float[4];
 			specColor[0] = mat.GetColor("_SpecColor").r;
 			specColor[1] = mat.GetColor("_SpecColor").g;
 			specColor[2] = mat.GetColor("_SpecColor").b;
 			specColor[3] = mat.GetColor("_SpecColor").a;
+			mat.shader = Shader.Find("Standard");
+
 			// Glossiness / Smoothness Value
-			float glossiness = mat.GetFloat("_Glossiness");			
-			
-			// Look up Ns and Ks in the corresponding .mtl file 
+			float glossiness = mat.GetFloat("_Glossiness");		
+
+			// Metallicness
+			float metallicness = mat.GetFloat("_Metallic");	
+
+
+			// Look up Ka, Kd, Ks, map_Kd, map_Ks, map_bump, d, and Ns in the corresponding .mtl file 
 			StreamReader reader = new StreamReader(mtlfile);
 			string contents = reader.ReadToEnd();
 			Match m = Regex.Match(contents, 
@@ -63,40 +77,96 @@ public class MaterialPropertyExtractor {
 
 			// Determine Ns
 			float Ns = 0.0f;
-			string NsString = LookUpMtlPrefix("Ns", mtlContents);
-			if(NsString != null)
-				float.TryParse(NsString, out Ns);
-			else
-				Debug.LogError("No Ns parameter found!");
+			bool hasNs = ParseMtlScalar("Ns", ref Ns, mtlContents);
+
+			// Determine d
+			float d = 0.0f;
+			bool hasD = ParseMtlScalar("d", ref d, mtlContents);
+
+			// Look up map_Kd
+			bool hasMapKd = LookUpMtlPrefix("map_Kd", mtlContents);
+
+			// Look up map_Ks
+			bool hasMapKs = LookUpMtlPrefix("map_Ks", mtlContents);
+
+			// Look up map_bump
+			bool hasMapBump = LookUpMtlPrefix("map_bump", mtlContents) || LookUpMtlPrefix("bump", mtlContents);
+
+			// Determine Ka
+			float[] Ka = new float[3];
+			bool hasKa = ParseMtlVector3D("Ka", ref Ka, mtlContents);
+
+			// Determine Ks
+			float[] Kd = new float[3];
+			bool hasKd = ParseMtlVector3D("Kd", ref Kd, mtlContents);
 
 			// Determine Ks
 			float[] Ks = new float[3];
-			Match m_Ks = Regex.Match(mtlContents, string.Format(
-				"{0}\\s+(?<r>\\d*\\.?\\d+)\\s+(?<g>\\d*\\.?\\d+)\\s+(?<b>\\d*\\.?\\d+)\\b", 
-				"Ks"));
-        	if (!m_Ks.Success) {
-				Debug.LogError("No Ks parameter found!");
-            	return;
-            }
-			Ks[0] = float.Parse(m_Ks.Groups["r"].Value);
-			Ks[1] = float.Parse(m_Ks.Groups["g"].Value);
-			Ks[2] = float.Parse(m_Ks.Groups["b"].Value);
+			bool hasKs = ParseMtlVector3D("Ks", ref Ks, mtlContents);
+
 
 			// Write Ks, Ns, specular Color and smoothness into file
 			output.WriteLine(
-				string.Format("{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}",
+				string.Format("{0}, {1}, {2}, {3}, {4}, {5}, " +
+							  "{6}, {7}, {8}, {9}, {10}, {11}, " +
+							  "{12}, {13}, {14}, {15}, {16}, {17}, " +
+							  "{18}, {19}, {20}, {21}, {22}, {23}" ,
+					Ka[0], Ka[1], Ka[2],
+					Kd[0], Kd[1], Kd[2],
 					Ks[0], Ks[1], Ks[2],
-					Ns,
-					specColor[0], specColor[1], specColor[2],
-					glossiness));
+					Ns, d, 
+					hasMapBump ? 1 : 0, 
+					hasMapKd ? 1 : 0, 
+					hasMapKs ? 1 : 0,
+					Color[0], Color[1], Color[2], Color[3],
+					specColor[0], specColor[1], specColor[2], specColor[3],
+					glossiness, metallicness));
 		}
 		output.Close();
 		Debug.Log("Output written to: " + output_path);
 	}
 
-	private static string LookUpMtlPrefix(string mtlKey, string mtlContents)
+	private static bool ParseMtlScalar(string mtlKey, ref float val, string mtlContents)
     {
+    	val = -1.0f;
         Match m = Regex.Match(mtlContents, string.Format("{0}\\s+(?<val>\\S+)\\b", mtlKey));
-        return m.Success ? m.Groups["val"].Value : null;
+		string valString = m.Success ? m.Groups["val"].Value : null;
+
+		if(valString != null) 
+		{
+			float.TryParse(valString, out val);
+			return true; 
+		}
+		else
+		{
+			Debug.LogError("No " + mtlKey + " parameter found!");
+			return false;
+		}
     }
+
+	private static bool ParseMtlVector3D(string mtlKey, ref float[] val, string mtlContents)
+    {
+		val[0] = -1.0f;
+		val[1] = -1.0f;
+		val[2] = -1.0f;
+
+		Match m = Regex.Match(mtlContents, string.Format(
+				"{0}\\s+(?<r>\\d*\\.?\\d+)\\s+(?<g>\\d*\\.?\\d+)\\s+(?<b>\\d*\\.?\\d+)\\b", 
+				mtlKey));
+        if (!m.Success) 
+        {
+			Debug.LogError("No " + mtlKey + " parameter found!");
+            return false;
+        }
+		val[0] = float.Parse(m.Groups["r"].Value);
+		val[1] = float.Parse(m.Groups["g"].Value);
+		val[2] = float.Parse(m.Groups["b"].Value);
+		return true;
+    }
+
+    private static bool LookUpMtlPrefix(string mtlKey, string mtlContents)
+    {
+		Match m = Regex.Match(mtlContents, string.Format("{0}\\s+(?<val>\\S+)\\b", mtlKey));
+		return m.Success;
+	}
 }
